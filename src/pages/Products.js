3d36ae127cfd6023acee9a6a104ce4b1a4bdbc9e
Santiago Cano <santiagocano6@hotmail.com>
2019-06-02 21:37:38 -0500

    Added material-design-icons

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -12,12 +12,23 @@ class Products extends React.Component {
                     <div className="product-categories-head">
                         <div className="product-categories-tittle">Categories</div>
                         <div className="product-categories-item selectable-item">
-                            <img src="" alt=""/>
+                            <i class="material-icons product-categories-item-icon" id="scr">list</i>
                             All
                         </div>
                     </div>
-                    <div className="product-categories-list selectable-item">
-                        Tech
+                    <div className="product-categories-list">
+                    <div className="product-categories-item selectable-item">
+                            <i class="material-icons product-categories-item-icon" id="scr">laptop</i>
+                            Tech
+                        </div>
+                        <div className="product-categories-item selectable-item">
+                            <i class="material-icons product-categories-item-icon" id="scr">build</i>
+                            build
+                        </div>
+                        <div className="product-categories-item selectable-item">
+                            <i class="material-icons product-categories-item-icon" id="scr">folder</i>
+                            Office
+                        </div>
                     </div>
                     
                 </div>
