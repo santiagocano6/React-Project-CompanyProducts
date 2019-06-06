68507b7719b4f67d53f4867f57be8986649f37c1
Santiago Cano <santiagocano6@hotmail.com>
2019-06-06 11:54:43 -0500

    Fix deployment

---
+++ build/precache-manifest.6decd3a272e437beb0e689a5236f3413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2afd20e1c5bc6d539febfd5da922ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19240c4d0dd600e3bc72",
+    "url": "/static/css/main.ab47f65e.chunk.css"
+  },
+  {
+    "revision": "124502b3ca7c8cf505a2",
+    "url": "/static/js/2.cf5ca7e7.chunk.js"
+  },
+  {
+    "revision": "19240c4d0dd600e3bc72",
+    "url": "/static/js/main.10f31a71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
